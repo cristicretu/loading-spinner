1900d63cd0a220d2aedc24c19af06dfaa55312fd
Cristian Crețu <crisemcr@gmail.com>
2023-06-21 21:50:59 +0300

    Initial Commit

---
+++ loading-spinner/loading_spinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  loading_spinnerApp.swift
+//  loading-spinner
+//
+//  Created by Cristian Cretu on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct loading_spinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
